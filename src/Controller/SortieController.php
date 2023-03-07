62802aaf47c0ddf2c5703d55204e73062adf8dd1
Nicolas <nicolasroyer22@gmail.com>
2023-03-07 17:16:09 +0100

    Changement d'état + accueil + nav + sortieController

--- src/Controller/SortieController.php
+++ src/Controller/SortieController.php
@@ -2,9 +2,14 @@
 
 namespace App\Controller;
 
-use App\Entity\Sortie;
 use App\Form\SortieType;
+use App\Repository\EtatRepository;
+use App\Repository\UserRepository;
+use App\Repository\CampusRepository;
 use App\Repository\SortieRepository;
+
+use App\Entity\Sortie;
+use App\Utils\ChangerEtat;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -13,6 +18,76 @@ use Symfony\Component\Routing\Annotation\Route;
 #[Route('/sortie', name: 'sortie_')]
 class SortieController extends AbstractController
 {
+    #[Route('', name: 'list')]
+    #[Route('/list', name: 'list')]
+    public function list(): Response
+    {
+        return $this->render('sortie/list.html.twig'
+
+        );
+    }
+
+    #[Route('/recherche', name: 'recherche')]
+    public function rechercheParFiltre(
+        CampusRepository   $campusRepository,
+        SortieRepository $sortieRepository,
+        ChangerEtat      $changerEtat,
+
+    ): Response
+
+    {
+
+        $choixCampus = filter_input(INPUT_POST, 'Campus-select', FILTER_SANITIZE_STRING);
+
+        $choixRecherche = filter_input(INPUT_POST, 'rechercher', FILTER_SANITIZE_STRING);
+
+        $choixDateDebut = filter_input(INPUT_POST, 'debut-sortie', FILTER_SANITIZE_STRING);
+
+        $choixDateFin = filter_input(INPUT_POST, 'fin-sortie', FILTER_SANITIZE_STRING);
+
+        $choixOrganisateur = filter_input(INPUT_POST, 'organisateur', FILTER_VALIDATE_INT);
+
+        $choixInscrit = filter_input(INPUT_POST, 'inscrit', FILTER_VALIDATE_INT);
+
+        $choixPasInscrit = filter_input(INPUT_POST, 'pasInscrit', FILTER_VALIDATE_INT);
+
+        $choixPassee = filter_input(INPUT_POST, 'passee', FILTER_SANITIZE_STRING);
+
+
+        if ($choixCampus != 'Tous') {
+            $leSiteId = $campusRepository->findOneBy(['nom' => $choixCampus]);
+            $leSiteId = $leSiteId->getId();
+        } else {
+            $leSiteId = -1;
+        }
+        if ((($choixDateDebut != null) and ($choixDateFin == null)) or (($choixDateFin != null) and $choixDateDebut == null)) {
+            $this->addFlash('error', 'Veuillez sélectionner les deux dates');
+            $sorties = $sortieRepository->findAll();
+
+        } else {
+            $sorties = $sortieRepository->selectSortiesAvecFiltres($leSiteId, $choixRecherche, $choixDateDebut, $choixDateFin,
+                $choixOrganisateur, $choixInscrit, $choixPasInscrit, $choixPassee);
+        }
+
+        $changerEtat->verifierEtat();
+        $sites = $campusRepository->findAll();
+
+        return $this->render('sortie/list.html.twig', [
+            "sortie" => $sorties,
+            "campus" => $sites,
+            "leSite" => $choixCampus,
+            "choixRecherche" => $choixRecherche,
+            "choixDateDebut" => $choixDateDebut,
+            "choixDateFin" => $choixDateFin,
+            "choixOrganisateur" => $choixOrganisateur,
+            'choixInscrit' => $choixInscrit,
+            'choixPasInscrit' => $choixPasInscrit,
+            'choixPassee' => $choixPassee,
+        ]);
+    }
+
+
+
     #[Route('/list', name: 'list', methods: 'GET')]
     public function sortie(): Response
     {
